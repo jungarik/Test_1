dff5a84c291becc888c9fc4b754ffa94e8e89c21
jungarik <gor132@i.ua>
2016-10-23 23:26:06 +0300

    Done: List, Details for Owners

--- CarOwners.Web/Controllers/HomeController.cs
+++ CarOwners.Web/Controllers/HomeController.cs
@@ -11,118 +11,114 @@ namespace CarOwners.Web.Controllers
 {
     public class HomeController : Controller
     {
-        UoW unitOfWork;
         public HomeController()
-        {
-            unitOfWork = new UoW();
-        }
+        {}
 
         public ActionResult Index()
         {
-            var owners = unitOfWork.Owners.GetAll().Include(o =>o.Cars).ToList();
-            return View(owners);
+            return View();
         }
 
-        public ActionResult Create()
-        {
-            return View("Edit", new Owner());
-        }
-        public PartialViewResult Add(int id = 1)
-        {
-            var owner = unitOfWork.Owners.GetAll().Include(o => o.Cars).FirstOrDefault(m => m.Id == id);
-            owner.Cars.Add(new Car { Name = "Name", CarModel = "Model"});
-            //Car car = new Car();
-            //unitOfWork.Cars.Create(car);
-            return PartialView("_Add",owner);
-        }
-
-        //[HttpPost]
-        //public ActionResult Add(Car car, int id)
+        //public ActionResult Create()
         //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        unitOfWork.Cars.Create(car);
-        //        Owner owner = unitOfWork.Owners.Get(id);
-        //        owner.Cars.Add(car);
-        //        unitOfWork.Owners.Update(owner);
-        //        unitOfWork.Save();
-        //        return RedirectToAction("Edit");
-        //    }
-        //    return RedirectToAction("Index");
+        //    return View("Edit", new Owner());
         //}
+        //public PartialViewResult Add(int id = 1)
+        //{
+        //    var owner = unitOfWork.Owners.GetAll().Include(o => o.Cars).FirstOrDefault(m => m.Id == id);
+        //    owner.Cars.Add(new Car { Name = "Name", CarModel = "Model"});
+        //    //Car car = new Car();
+        //    //unitOfWork.Cars.Create(car);
+        //    return PartialView("_Add",owner);
+        //}
+
+        ////[HttpPost]
+        ////public ActionResult Add(Car car, int id)
+        ////{
+        ////    if (ModelState.IsValid)
+        ////    {
+        ////        unitOfWork.Cars.Create(car);
+        ////        Owner owner = unitOfWork.Owners.Get(id);
+        ////        owner.Cars.Add(car);
+        ////        unitOfWork.Owners.Update(owner);
+        ////        unitOfWork.Save();
+        ////        return RedirectToAction("Edit");
+        ////    }
+        ////    return RedirectToAction("Index");
+        ////}
+        ////[HttpPost]
+        ////public ActionResult Create(Owner owner)
+        ////{
+        ////    if (ModelState.IsValid)
+        ////    {
+        ////        unitOfWork.Owners.Create(owner);
+        ////        unitOfWork.Save();
+        ////        return RedirectToAction("Index");
+        ////    }
+        ////    return View(owner);
+        ////}
+
+        //public ActionResult Edit(int id = 1)
+        //{
+        //    Owner owner = unitOfWork.Owners.GetAll().Include(o =>o.Cars).FirstOrDefault(o => o.Id == id);
+        //    //Owner owner = unitOfWork.Owners.Get(id);
+        //    if (owner == null)
+        //        return HttpNotFound();
+        //    return View(owner);
+        //}
+
         //[HttpPost]
-        //public ActionResult Create(Owner owner)
+        //public ActionResult Edit(Owner owner, Car[] cars)
         //{
         //    if (ModelState.IsValid)
         //    {
-        //        unitOfWork.Owners.Create(owner);
-        //        unitOfWork.Save();
-        //        return RedirectToAction("Index");
+        //        //if (owner.Id == 0)
+        //        //{
+        //        //    if (cars != null)
+        //        //    {
+        //        //        foreach (var car in cars)
+        //        //        {
+        //        //            unitOfWork.Cars.Create(car);
+        //        //        }
+        //        //        owner.Cars = cars.ToList<Car>();
+        //        //    }
+        //        //    unitOfWork.Owners.Create(owner);
+        //        //    unitOfWork.Save();
+        //        //    return RedirectToAction("Index");
+        //        //}
+        //        //else
+        //        {
+        //            //Если была добавлена новая машина создать ее в базе
+        //            if (cars.Last().Id == 0)
+        //            {
+        //                unitOfWork.Cars.Create(cars.Last());
+        //                unitOfWork.Save();
+        //                owner.Cars = cars.ToList();
+        //            }
+
+        //            unitOfWork.Owners.Update(owner);
+        //            foreach (var c in cars)
+        //            {
+        //                unitOfWork.Cars.Update(c);
+        //            }
+        //            unitOfWork.Save();
+        //            return RedirectToAction("Index");
+        //        }
         //    }
         //    return View(owner);
         //}
 
-        public ActionResult Edit(int id = 1)
-        {
-            Owner owner = unitOfWork.Owners.GetAll().Include(o =>o.Cars).FirstOrDefault(o => o.Id == id);
-            //Owner owner = unitOfWork.Owners.Get(id);
-            if (owner == null)
-                return HttpNotFound();
-            return View(owner);
-        }
+        //public ActionResult Delete(int id)
+        //{
+        //    unitOfWork.Owners.Delete(id);
+        //    unitOfWork.Save();
+        //    return RedirectToAction("Index");
+        //}
 
-        [HttpPost]
-        public ActionResult Edit(Owner owner, Car[] cars)
-        {
-            if (ModelState.IsValid)
-            {
-                //if (owner.Id == 0)
-                //{
-                //    if (cars != null)
-                //    {
-                //        foreach (var car in cars)
-                //        {
-                //            unitOfWork.Cars.Create(car);
-                //        }
-                //        owner.Cars = cars.ToList<Car>();
-                //    }
-                //    unitOfWork.Owners.Create(owner);
-                //    unitOfWork.Save();
-                //    return RedirectToAction("Index");
-                //}
-                //else
-                {
-                    //Если была добавлена новая машина создать ее в базе
-                    if (cars.Last().Id == 0)
-                    {
-                        unitOfWork.Cars.Create(cars.Last());
-                        unitOfWork.Save();
-                        owner.Cars = cars.ToList();
-                    }
-
-                    unitOfWork.Owners.Update(owner);
-                    foreach (var c in cars)
-                    {
-                        unitOfWork.Cars.Update(c);
-                    }
-                    unitOfWork.Save();
-                    return RedirectToAction("Index");
-                }
-            }
-            return View(owner);
-        }
-
-        public ActionResult Delete(int id)
-        {
-            unitOfWork.Owners.Delete(id);
-            unitOfWork.Save();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            unitOfWork.Dispose();
-            base.Dispose(disposing);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    unitOfWork.Dispose();
+        //    base.Dispose(disposing);
+        //}
     }
 }
---
+++ CarOwners.Web/Controllers/OwnersController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using CarOwners.Domain.Entities;
+using CarOwners.Domain.Concrete;
+
+namespace CarOwners.Web.Controllers
+{
+    public class OwnersController : Controller
+    {
+        UoW unitOfWork;
+        public OwnersController()// Здесь надо использовать DI Ninj
+        {
+            unitOfWork = new UoW();//Вместо этого
+        }
+        // GET: Owners
+        public ActionResult List()
+        {
+            var owners = unitOfWork.Owners.GetAll().Include(o => o.Cars).ToList();
+            return View(owners);
+        }
+
+        public ActionResult Details(int id = 1)
+        {
+            Owner owner = unitOfWork.Owners.GetAll().Include(o => o.Cars).FirstOrDefault(o => o.Id == id);
+            return View(owner);
+        }
+        public ActionResult Edit(int id = 1)
+        {
+            Owner owner = unitOfWork.Owners.GetAll().Include(o =>o.Cars).FirstOrDefault(o => o.Id == id);
+            //Owner owner = unitOfWork.Owners.Get(id);
+            if (owner == null)
+                return HttpNotFound();
+            return View(owner);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Owner owner, Car[] cars)
+        {
+            if (ModelState.IsValid)
+            {
+                //if (owner.Id == 0)
+                //{
+                //    if (cars != null)
+                //    {
+                //        foreach (var car in cars)
+                //        {
+                //            unitOfWork.Cars.Create(car);
+                //        }
+                //        owner.Cars = cars.ToList<Car>();
+                //    }
+                //    unitOfWork.Owners.Create(owner);
+                //    unitOfWork.Save();
+                //    return RedirectToAction("Index");
+                //}
+                //else
+                {
+                    //Если была добавлена новая машина создать ее в базе
+                    if (cars.Last().Id == 0)
+                    {
+                        unitOfWork.Cars.Create(cars.Last());
+                        unitOfWork.Save();
+                        owner.Cars = cars.ToList();
+                    }
+
+                    unitOfWork.Owners.Update(owner);
+                    foreach (var c in cars)
+                    {
+                        unitOfWork.Cars.Update(c);
+                    }
+                    unitOfWork.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(owner);
+        }
+    }
+}
